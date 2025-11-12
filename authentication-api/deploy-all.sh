507efccddfa8054dacd681feb25b12c5d3e327d9
kaiqkt <kaiquebeserra1313@gmail.com>
2025-11-12 14:16:50 -0300

    refactor: update project structure and configuration

--- authentication-api/deploy-all.sh
+++
@@ -1,5 +0,0 @@
-docker stack deploy --compose-file authentication-api/dependencies-stack.yml authentication_dependencies
-
-sleep 10
-
-docker stack deploy --compose-file authentication-api/stack.yml authentication
--- external-gateway-api/deploy-all.sh
+++
@@ -1,5 +0,0 @@
-docker stack deploy --compose-file external-gateway-api/dependencies-stack.yml gateway_dependencies
-
-sleep 10
-
-docker stack deploy --compose-file external-gateway-api/stack.yml gateway
